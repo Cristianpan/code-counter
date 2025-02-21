2711a161c84f46d65d690745bb95872218f775c5
Paulina <a21216402@alumnos.uady.mx>
2025-02-20 22:04:48 -0600

    Primera entrega, clases agregadas CodeReader y FileReader. Clases modificadas Main. Resolviendo issue #1

--- src/main/java/mantenimiento/codecounter/Main.java
+++ src/main/java/mantenimiento/codecounter/Main.java
@@ -1,7 +1,11 @@
 package mantenimiento.codecounter;
+import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Init application...");
+        String directoryPath = "C:\\Users\\pauli\\Downloads\\Mantenimiento 3.0\\ejemplo"; 
+        FileReader fileReader = new FileReader(directoryPath);
+        fileReader.readFiles();
     }
 }
