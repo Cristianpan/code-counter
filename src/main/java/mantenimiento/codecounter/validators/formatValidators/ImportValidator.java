c2c2fea3fee2f59147fd9f545f44422d20b640d7
cristianpan <panzaldivarcristian@gmail.com>
2025-02-23 16:57:41 -0600

    docs: :memo: Add documentation in the import validator

--- src/main/java/mantenimiento/codecounter/validators/formatValidators/ImportValidator.java
+++ src/main/java/mantenimiento/codecounter/validators/formatValidators/ImportValidator.java
@@ -2,12 +2,28 @@ package mantenimiento.codecounter.validators.formatValidators;
 
 import mantenimiento.codecounter.exceptions.InvalidFormatException;
 
+/**
+ * Clase que valida si la importacion
+ * de un paquete tiene un comodin y lanza una excepcion
+ * en caso de haber
+ * 
+ * ej: import java.util*
+ * 
+ */
 public class ImportValidator extends FormatValidator {
 
     public ImportValidator() {
         super();
     }
 
+    /**
+     * Realiza la validacion del formato verificando si 
+     * es un import y si utiliza un comodin
+     * 
+     * @param lineOfFile linea de texto (sentencia de codigo) a validar
+     * @return true si el import es sin comodin
+     * @throws InvalidFormatException si el import se realiza con comodin
+     */
     @Override
     public boolean isValid(String lineOfFile) throws InvalidFormatException {
         if (isImportDeclaration(lineOfFile)) {
@@ -21,11 +37,24 @@ public class ImportValidator extends FormatValidator {
         return this.validateNext(lineOfFile);
     }
 
+    /**
+     * Valida si la sentencia corresponde a una
+     * declaracion de import
+     * 
+     * @param lineOfCode sentencia a validar
+     * @return true si es una declaracion de import, false si no corresponde
+     */
     private boolean isImportDeclaration(String lineOfCode) {
         String importDeclarationRegex = "^import\\s+.*;$";
         return lineOfCode.matches(importDeclarationRegex);
     }
 
+    /**
+     * Valida si la declaracion de import, incluso un import statico, tiene comodín
+     * 
+     * @param lineOfCode sentencia a validar
+     * @return true si el import es con comodin, false si no corresponde
+     */
     private boolean isImportWithWildcard(String lineOfCode) {
         String wildcardImportRegex = "^import\\s+(static\\s+)?(\\w+\\.\\s*)+\\*\\s*;";
         return lineOfCode.matches(wildcardImportRegex);
