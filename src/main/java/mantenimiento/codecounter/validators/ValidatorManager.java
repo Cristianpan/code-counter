ab500cc7589f0611328b16c6d443d378dd28124d
cristianpan <panzaldivarcristian@gmail.com>
2025-03-04 10:22:28 -0600

    feat: :sparkles: Add validator for lambda declaration with JUnit test

--- src/main/java/mantenimiento/codecounter/validators/ValidatorManager.java
+++ src/main/java/mantenimiento/codecounter/validators/ValidatorManager.java
@@ -7,6 +7,7 @@ import mantenimiento.codecounter.validators.formatValidators.SingleAnnotationVal
 import mantenimiento.codecounter.validators.formatValidators.SingleDeclarationValidator;
 import mantenimiento.codecounter.validators.formatValidators.StyleKAndRValidator;
 import mantenimiento.codecounter.validators.logicalValidators.FlowControlDeclarationValidator;
+import mantenimiento.codecounter.validators.logicalValidators.LambdaDeclarationValidator;
 import mantenimiento.codecounter.validators.logicalValidators.MethodDeclarationValidator;
 import mantenimiento.codecounter.validators.logicalValidators.TryDeclarationValidator;
 import mantenimiento.codecounter.validators.logicalValidators.TypeDeclarationValidator;
@@ -56,10 +57,12 @@ public class ValidatorManager {
         LogicalValidatorHandler flowControlDeclarationValidator = new FlowControlDeclarationValidator();
         LogicalValidatorHandler typeDeclarationValidator = new TypeDeclarationValidator(); 
         LogicalValidatorHandler methodDeclarationValidator = new MethodDeclarationValidator(); 
+        LogicalValidatorHandler lambdaDeclarationValidator = new LambdaDeclarationValidator(); 
         
         tryCatchValidator.setNextValidator(flowControlDeclarationValidator);
         flowControlDeclarationValidator.setNextValidator(typeDeclarationValidator);
         typeDeclarationValidator.setNextValidator(methodDeclarationValidator);
+        typeDeclarationValidator.setNextValidator(lambdaDeclarationValidator);
         logicalValidator = tryCatchValidator;
 
         return logicalValidator;
